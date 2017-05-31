dddfac44617b15b19206bf61af371e0b5946b243
caionakai <kaioesperto@hotmail.com>
2017-05-31 15:12:59 -0300

    add

--- Projeto BD1.sql
+++ Projeto BD1.sql
@@ -1,11 +1,17 @@
 drop schema if exists projetobd;
 create schema projetobd;
 use projetobd;
-
+create table Faixa_Etaria(
+	codigo integer primary key,
+    idade varchar(30)
+);
 create table obra(
     codigo integer primary key,     
-    titulo varchar (30),  
-    data_lançamento date
+    titulo varchar (30),
+    sinopse varchar(300),
+    Faixa_Etaria_codigo integer not null, 
+    data_lançamento date,
+    foreign key(Faixa_Etaria_codigo) references Faixa_Etaria(codigo)
     
 );
 create table genero(
@@ -21,52 +27,63 @@ create table genero_obra(
 );
 create table filme(
     obra_codigo integer not null,
+    trailer varchar(300),
     foreign key (obra_codigo) references obra(codigo)
 );
 
 create table serie(
-    codigo integer primary key,
-    obra_codigo integer not null,
+	stattus varchar(100),
+    obra_codigo integer primary key,
     foreign key (obra_codigo) references obra(codigo)
 );
 
 create table temporada(
-    numero integer primary key,
-    serie_codigo integer not null,
-    foreign key(serie_codigo) references serie(codigo)
+    numero integer,
+    n_episodios integer,
+    trailer varchar(300),
+    serie_codigo integer,
+    constraint temporada_key primary key(numero,serie_codigo),
+    foreign key(serie_codigo) references serie(obra_codigo)
 );
 
 create table episodio(
-    nome varchar(100) primary key,
-    numero integer not null,
-    foreign key(numero) references temporada(numero)
+    nome varchar(100),
+    numero integer,
+    sinopse varchar(200),
+    constraint episodio_key primary key(nome,numero),
+    temporada_numero integer not null,
+    foreign key(temporada_numero) references temporada(numero)
 );
 create table usuario(
-	id integer primary key
+	id integer primary key,
+    nome varchar(30),
+    senha varchar(20),
+    email varchar(50) unique
 );
 create table administrador(
 	usuario_id integer not null,
-    id integer not null,
-    primary key(usuario_id,id),
+    codigo integer not null,
+    primary key(usuario_id,codigo),
     foreign key(usuario_id) references usuario(id)
 );
-create table assistido(
+create table assistiu(
 	usuario_id integer not null,
     obra_codigo integer not null,
-    quando date,
-    primary key(usuario_id,obra_codigo),
+    datta datetime,
+    primary key(usuario_id,obra_codigo, datta),
     foreign key(usuario_id) references usuario(id),
     foreign key (obra_codigo) references obra(codigo)
 );
 create table deseja_assistir(
 	usuario_id integer not null,
     obra_codigo integer not null,
-    primary key(usuario_id,obra_codigo),
+    datta datetime,
+    primary key(usuario_id,obra_codigo, datta),
     foreign key(usuario_id) references usuario(id),
     foreign key (obra_codigo) references obra(codigo)
 );
 
-create table favorito(
+create table favorita(
 	usuario_id integer not null,
     obra_codigo integer not null,
     primary key(usuario_id,obra_codigo),
@@ -74,10 +91,12 @@ create table favorito(
     foreign key (obra_codigo) references obra(codigo)
 );
 
-create table ranking(
+create table comentario(
 	usuario_id integer not null,
     obra_codigo integer not null,
-    primary key(usuario_id,obra_codigo),
+    datta datetime not null,
+    texto varchar(300),
+    primary key(usuario_id,obra_codigo, datta),
     foreign key(usuario_id) references usuario(id),
     foreign key (obra_codigo) references obra(codigo)
 );
@@ -89,23 +108,36 @@ create table idioma(
     foreign key (obra_codigo) references obra(codigo)
 );
 create table legenda(
-	idioma varchar(30),
+	nome varchar(30),
     sigla_pais varchar(2),
     primary key(nome,sigla_pais),
 	obra_codigo integer not null,
     foreign key (obra_codigo) references obra(codigo)
 );
 create table personagem(
-	nome varchar(100),
-    primary key(nome),
+	nome varchar(100) primary key
+);
+
+create table personagem_obra(
+	personagem_nome varchar(100),
 	obra_codigo integer not null,
-    foreign key (obra_codigo) references obra(codigo)
+    primary key(personagem_nome, obra_codigo),
+    foreign key (obra_codigo) references obra(codigo),
+    foreign key (personagem_nome) references personagem(nome)
 );
 
 create table ator(
-	nome varchar(100),
-    codigo integer,
-    primary key(codigo),
+	codigo integer primary key,
+    nome varchar(100),
+    idade integer,
+    sexo varchar(20),
+    biografia varchar(300)
+);
+
+create table personagem_ator(
+	ator_codigo integer,
 	personagem_nome varchar(100) not null,
-    foreign key (personagem_nome) references personagem(nome)
+    primary key (ator_codigo, personagem_nome),
+    foreign key (personagem_nome) references personagem(nome),
+    foreign key (ator_codigo) references ator(codigo)
 );
