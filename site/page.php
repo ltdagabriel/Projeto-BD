c9d80c426dfad9558b67f549d953e987ba34dbe3
ltdagabriel <gabriel_choptian@hotmail.com>
2017-06-03 18:04:22 -0300

    ass

---
+++ site/page.php
@@ -0,0 +1,13 @@
+<?php
+// Aqui ele pega o valor da varíavel do navegador.
+$cod = $_GET['cod'];
+// A condição switch faz com que o valor dos códigos do primeiro script represente uma página html que carregará na div conteudo.
+switch($cod){
+case '1'; // Aqui por exemplo, lá é page.php?cod=1, certo? Então ela vai carregar a página home.html na div conteúdo.
+include('cadastro.html');
+break;
+case '2'; // Aqui tambem, lá é page.php?cod=2, então ela carregará a página de noticias.html.
+include('noticias.html');
+}
+
+?>
