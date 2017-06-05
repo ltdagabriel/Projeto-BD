68a00932cfcdc48f75fc0fbf3d0ad09415faf1e2
ltdagabriel <gabriel_choptian@hotmail.com>
2017-06-05 12:56:03 -0300

    hgf

--- site/index.php
+++ site/index.php
@@ -17,7 +17,7 @@
 <body>
 <div id="logo" class="col-lg-12 col-md-12 col-sm-12">
 	<div id="logo" class="logo col-lg-6 col-md-6 col-sm-6 text-center">
-		<a href="index.html"><h1>My Hobbie Filmes e Series</h1></a>
+		<a href="index.php"><h1>My Hobbie Filmes e Series</h1></a>
 	</div>
 	<div class=" espaco col-lg-4 col-md-4 col-sm-4">
 		<div class="logo col-lg-6 col-md-6 col-sm-6 text-center">
