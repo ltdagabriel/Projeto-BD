a37073ce4299c40f093fe2b92492b7bd851badc6
Gabriel Choptian <gabriel_choptian@hotmail.com>
2017-06-20 17:04:09 -0300

    asdas

--- site/classes/ConectBD/episodio.php
+++ site/classes/ConectBD/episodio.php
@@ -31,6 +31,19 @@ class episodioDAO {
             echo " Falha na inserção do episodio : {$ex->getMessage()} ";
         }
     }
+    public function Retorna_Todos($titulo_obra,$temp_numero){
+        try {
+            $stmt = $this->pdo->prepare("SELECT nome,numero,sinopse,video,foto FROM filme,obra WHERE :titulo = obra_titulo :temp_numero = temporada_numero");
+            $param = array(
+                ":titulo" => $titulo_obra,
+                ":temp_numero" => $temp_numero               
+            );
+            $stmt->execute($param);
+            return $stmt;
+        } catch (PDOException $ex) {
+            echo " Falha ao Retornar todos os Episodios : {$ex->getMessage()} \n";
+        }
+    }
 
 }
 
